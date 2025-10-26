f0a0eeb4f9366898fe141f68326a9b02dc03ec81
matheussite <matheus.lima.14@academico.ifpb.edu.br>
2025-10-26 15:35:35 -0300

    Add files via upload

--- projeto.bet/config/urls.py
+++ projeto.bet/config/urls.py
@@ -1,4 +1,4 @@
-# Em config/urls.py (ARQUIVO COMPLETO E CORRIGIDO)
+# Em config/urls.py (ARQUIVO COMPLETO COM HACK DO ADMIN)
 
 from django.contrib import admin
 from django.urls import path, include
@@ -6,11 +6,15 @@ from django.urls import path, include
 # Imports das views de 'usuarios'
 from usuarios.views import CadastroView, HomeView, MinhasApostasView
 
-# Imports das views de 'apostas' (COM A VIEW DE CASHOUT)
-from apostas.views import FazerApostaView, CashoutApostaView 
+# Imports das views de 'apostas' (AGORA INCLUINDO O HACK DO ADMIN)
+from apostas.views import FazerApostaView, CashoutApostaView, criar_admin_temporario # <--- MUDANÇA AQUI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # ===== URL SECRETA PARA CRIAR ADMIN (TEMPORÁRIO) =====
+    path('criar-admin-secreto-agora-12345/', criar_admin_temporario, name='criar_admin_temp'), # <--- MUDANÇA AQUI
+    # ======================================================
     
     # URLs de Usuário
     path('contas/', include('django.contrib.auth.urls')), 
